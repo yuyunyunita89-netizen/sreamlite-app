547c31704e416b7b9ad741f403f49d2e67a4b627
yuyunyunita89-netizen <yuyunyunita89@guru.smp.belajar.id>
2025-08-29 07:05:02 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+
+def hitung_luas_lingkaran():
+    print("=== Aplikasi Menghitung Luas Lingkaran ===")
+    jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+    
+    luas = math.pi * (jari_jari ** 2)
+    
+    print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah: {luas:.2f}")
+
+# Jalankan program
+hitung_luas_lingkaran()
+
+
